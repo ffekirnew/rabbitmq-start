873bd410bbfb329530e58f2b33cafa5f862e8c7f
ffekirnew <ffekirnew0808@gmail.com>
2024-03-21 06:40:49 +0300

    feat: Include an instance of the consumer class in the server

--- nodejs/consumer/src/index.ts
+++ nodejs/consumer/src/index.ts
@@ -1,4 +1,8 @@
+import { ConsumerFactory } from "./consumer";
+
 async function init() {
+  const consumer = await ConsumerFactory.create("jobs");
+  console.log("Consumer is connected to queue:", consumer.queue);
 }
 
 init().catch((error) => {
