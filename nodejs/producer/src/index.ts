ab8b058a201d65a523053bc11d34ba0cc328cd42
ffekirnew <ffekirnew0808@gmail.com>
2024-03-21 06:43:16 +0300

    feat: Integrate the producer on the server and accept a message body attribute on end point /publish

--- nodejs/producer/src/index.ts
+++ nodejs/producer/src/index.ts
@@ -1,11 +1,24 @@
 import express, { Express } from "express";
+import { Producer, ProducerFactory } from "./producer";
 import cors from "cors";
 
 const app: Express = express();
+let producer: Producer;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+async function init() {
+  producer = await ProducerFactory.create("jobs");
+  console.log("Producer is connected.");
+}
+
+app.post("/publish", (req, res) => {
+  producer.produce({ message: req.body.message });
+  res.send("Message has been sent!");
+});
+
 app.listen(3000, async () => {
   await init();
   console.log("Server is running on port: 3000.");
