d231283b3b37c6f896b50db8924e0f24fe665b35
ffekirnew <ffekirnew0808@gmail.com>
2024-03-21 10:29:14 +0300

    Integrate the producer to the server

--- python/producer/src/main.py
+++ python/producer/src/main.py
@@ -1,2 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from src.producer import ProducerFactory
+
+producer = ProducerFactory.create_producer("jobs")
 app = FastAPI()
+
+
+class Job(BaseModel):
+    name: str
+    description: str
+
+
+@app.post("/publish")
+def publish(job: Job):
+    producer.publish(job.model_dump())
+
+    return {"message": "Job published successfully!"}
