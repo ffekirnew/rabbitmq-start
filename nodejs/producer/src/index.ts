7a8b827c25b48dbc65fd4c6d0d5b74178ead687b
ffekirnew <ffekirnew0808@gmail.com>
2024-03-21 06:42:47 +0300

    Create an express server for publishing

--- nodejs/producer/src/index.ts
+++ nodejs/producer/src/index.ts
@@ -1,1 +1,12 @@
+import express, { Express } from "express";
+import cors from "cors";
 
+const app: Express = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.listen(3000, async () => {
+  await init();
+  console.log("Server is running on port: 3000.");
+});
